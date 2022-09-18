649eb73f92e7a1c2a2e258fbaec786f0bee9d52a
Jeff Andrews <babajeff8170@gmail.com>
2022-09-18 15:10:08 -0400

    finalized blog post application and CRUD functionality using Redux Toolkit

--- src/UserPost/index.js
+++ src/UserPost/index.js
@@ -5,12 +5,34 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Card, Input, Space } from 'antd';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchPost, deletePost, setEdit, updatePost } from '../redux/features/postSlice';
+import LoadingCard from './LoadingCard';
 
 const Home = () => {
     const [id, setId] = useState();
-    const fetchUserPost = () => {};
-    const createUserPost = () => {};
+    const [bodyText, setBodyText] = useState('');
+    const dispatch = useDispatch();
     const navigate = useNavigate();
+    const { loading, post, edit, body } = useSelector((state) => ({
+        ...state.app,
+    }));
+    console.log('post', post);
+    useEffect(() => {
+        if (body) {
+            setBodyText(body);
+        }
+    }, [body]); //only call if body changes
+
+    const fetchUserPost = () => {
+        if (!id) {
+            window.alert('please provide an id before proceeding!');
+        } else {
+            dispatch(fetchPost({ id }));
+            setId('');
+        }
+    };
+
     return (
         <div className="container">
             <h1 style={{ textAlign: 'center' }}>Fetch Post</h1>
@@ -27,8 +49,90 @@ const Home = () => {
                 <Button type="primary" onClick={fetchUserPost}>
                     Fetch User Post
                 </Button>
-                <Button type="primary" onClick={() => navigate("/createPost")}>Create User Post</Button>
+                <Button type="primary" onClick={() => navigate('/createPost')}>
+                    Create User Post
+                </Button>
             </Space>
+            <br />
+            <br />
+
+            {loading ? (
+                <LoadingCard count={1} />
+            ) : (
+                <>
+                    {post.length > 0 && (
+                        <div className="site-card-border-less-wrapper">
+                            <Card type="inner" title={post[0].title}>
+                                <p>User Id: {post[0].id}</p>
+                                {edit ? (
+                                    <>
+                                        <Input.TextArea
+                                            rows={4}
+                                            type="text"
+                                            onChange={(e) =>
+                                                setBodyText(e.target.value)
+                                            }
+                                            value={bodyText}
+                                        />
+                                        <Space>
+                                            <Button type="primary" onClick={() => {
+                                                dispatch(updatePost({
+                                                    id: post[0].id,
+                                                    title: post[0].title,
+                                                    body: bodyText,
+                                                }))
+                                                dispatch(setEdit({ edit: false, body: ""}))
+                                            }}>Save</Button>
+                                            <Button onClick={() => {
+                                                dispatch(setEdit({ edit: false, body: ""}))
+                                            }}>Cancel</Button>
+                                        </Space>
+                                    </>
+                                ) : (
+                                    <span>{post[0].body}</span>
+                                )}
+                            </Card>
+                            {!edit && (
+                                <Space
+                                    size="middle"
+                                    style={{
+                                        marginTop: 35,
+                                        marginLeft: 5,
+                                        float: 'right',
+                                    }}
+                                >
+                                    <Button
+                                        style={{ cursor: 'pointer' }}
+                                        type="primary"
+                                        danger
+                                        onClick={() =>
+                                            dispatch(
+                                                deletePost({ id: post[0].id })
+                                            )
+                                        }
+                                    >
+                                        Delete
+                                    </Button>
+                                    <Button
+                                        style={{ cursor: 'pointer' }}
+                                        type="primary"
+                                        onClick={() =>
+                                            dispatch(
+                                                setEdit({
+                                                    edit: true,
+                                                    body: post[0].body,
+                                                })
+                                            )
+                                        }
+                                    >
+                                        Edit
+                                    </Button>
+                                </Space>
+                            )}
+                        </div>
+                    )}
+                </>
+            )}
         </div>
     );
 };
--- src/redux/features/postSlice.js
+++ src/redux/features/postSlice.js
@@ -3,17 +3,54 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 //imports for slice are createSlice and createAsyncThunk for the api call
 
 //create an asyncThunk and a slice for each action that requires an api call
-export const createUserPost = createAsyncThunk('createPost', async () => {
-    return fetch('apiAddressCall', {
-        method: 'Post',
-    }).then((res) => res.json());
-});
+export const createPost = createAsyncThunk(
+    'post/createPost',
+    async ({ values }) => {
+        return fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                title: values.title,
+                body: values.body,
+            }),
+        }).then((res) => res.json());
+    }
+);
 
-export const fetchPost = createAsyncThunk('getPost', async ({ id }) => {
+export const fetchPost = createAsyncThunk('post/getPost', async ({ id }) => {
     return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then(
         (res) => res.json()
     );
 });
+
+export const updatePost = createAsyncThunk(
+    'post/updatePost',
+    async ({ id, title, body }) => {
+        return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+            method: 'PUT',
+            headers: {
+                Accept: 'application/json',
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                title,
+                body,
+            }),
+        }).then((res) => res.json());
+    }
+);
+
+export const deletePost = createAsyncThunk(
+    'post/deletePost',
+    async ({ id }) => {
+        return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+            method: 'DELETE',
+        }).then((res) => res.json());
+    }
+);
 
 export const postSlice = createSlice({
     name: 'post',
@@ -21,8 +58,18 @@ export const postSlice = createSlice({
         post: [],
         loading: false,
         error: null,
+        body: '', //need a place to store the body for updating, and an edit mode so that the app knows the post is to be edited.
+        edit: false,
     },
-    extraReducers: {// three promise lifecycle is handled by each of the three extra reducers for each fetch call
+    reducers: {
+        // these work just like reducers the old way of using redux..
+        setEdit: (state, action) => {
+            state.edit = action.payload.edit;//here, we are getting an altering of the body text and the state of the edit mode associated with updating a single post
+            state.body = action.payload.body;
+        },
+    },
+    extraReducers: {
+        // three promise lifecycle is handled by each of the three extra reducers for each fetch call
         [fetchPost.pending]: (state, action) => {
             state.loading = true;
         },
@@ -34,7 +81,42 @@ export const postSlice = createSlice({
             state.loading = false;
             state.error = action.payload;
         },
+        [deletePost.pending]: (state, action) => {
+            state.loading = true;
+        },
+        [deletePost.fulfilled]: (state, action) => {
+            state.loading = false;
+            state.post = action.payload;
+        },
+        [deletePost.rejected]: (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        },
+        [createPost.pending]: (state, action) => {
+            state.loading = true;
+        },
+        [createPost.fulfilled]: (state, action) => {
+            state.loading = false;
+            state.post = [action.payload];
+        },
+        [createPost.rejected]: (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        },
+        [updatePost.pending]: (state, action) => {
+            state.loading = true;
+        },
+        [updatePost.fulfilled]: (state, action) => {
+            state.loading = false;
+            state.post = [action.payload];
+        },
+        [updatePost.rejected]: (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        },
     },
 });
+
+export const { setEdit } = postSlice.actions;//interestingly, you export setEdit not from the reducers property, but an 'actions' property
 
 export default postSlice.reducer;
